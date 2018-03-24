59d2ec1e145499baab93672a5aa9c38c239967d5
Christoffer A Traeen <christoffer.andersen@live.com>
2018-03-24 15:27:30 +0100

    Add more assets to cache

--- sw.js
+++ sw.js
@@ -1,35 +1,35 @@
 var staticCacheName = 'restaurant-static-1'
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll(['/', 'js/main.js', 'css/styles.css'])
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(['/', 'restaurant.html', 'js/main.js', 'js/dbhelper.js', 'js/serviceworkerRegister.js', 'js/restaurant_info.js', 'css/styles.css', 'data/restaurants.json']);
     })
   )
 })
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
         cacheNames
-          .filter(function(cacheName) {
-            return (
-              cacheName.startsWith('restaurant-static-') && cacheName != staticCacheName
-            )
-          })
-          .map(function(cacheName) {
-            return caches.delete(cacheName)
-          })
+        .filter(function (cacheName) {
+          return (
+            cacheName.startsWith('restaurant-static-') && cacheName != staticCacheName
+          )
+        })
+        .map(function (cacheName) {
+          return caches.delete(cacheName)
+        })
       )
     })
   )
 })
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request)
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
     })
   )
 })
