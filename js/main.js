7e2e1e0d6c77800c53d790523be779cbbe8a7351
Christoffer A Traeen <christoffer.andersen@live.com>
2018-03-25 16:05:22 +0200

    Add alt text on images

--- js/main.js
+++ js/main.js
@@ -140,7 +140,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.setAttribute('alt', '')  
+  image.setAttribute('alt', restaurant.name + ' restaurant')  
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
