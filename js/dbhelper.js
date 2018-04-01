559a79beaa2f4caf77a2266f11c6d190376c515d
Christoffer A Traeen <christoffer.andersen@live.com>
2018-04-01 13:14:46 +0200

    Feature: Add Fetch api for getting restaurants

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,27 +8,28 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 7000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337 // Change this to your server port
+    return `http://127.0.0.1:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
+    fetch(DBHelper.DATABASE_URL)
+      .then(function (response) {
+        if (response.ok) {
+          return response
+        }
+        return Promise.reject(response.status + ": " + response.statusText)
+      })
+      .then(response => response.json())
+      .then(function (data) {
+        callback(null, data);
+      })
+      .catch((error) => {
         callback(error, null);
-      }
-    };
-    xhr.send();
+      });
   }
 
   /**
@@ -162,8 +163,8 @@ class DBHelper {
       title: restaurant.name,
       url: DBHelper.urlForRestaurant(restaurant),
       map: map,
-      animation: google.maps.Animation.DROP}
-    );
+      animation: google.maps.Animation.DROP
+    });
     return marker;
   }
 
