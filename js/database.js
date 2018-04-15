e3139ddf43e985af778f9a42e980bfb207830565
Christoffer A Traeen <christoffer.andersen@live.com>
2018-04-15 23:11:47 +0200

    Add more indexes to datastore

--- js/database.js
+++ js/database.js
@@ -5,14 +5,19 @@ var idb = require('idb');
  * Setup indexes
  * 
  */
-let db = idb.open('restaurant', 1, function (upgradeDB) {
+let db = idb.open('restaurant', 2, function (upgradeDB) {
   switch (upgradeDB.oldVersion) {
     case 0:
       upgradeDB.createObjectStore('restaurants', {
         keyPath: 'id'
       });
-      var restaurantStore = upgradeDB.transaction.objectStore('restaurants');
+      let restaurantStore = upgradeDB.transaction.objectStore('restaurants');
       restaurantStore.createIndex('id', 'id');
+    case 1:
+      let restaurantStore2 = upgradeDB.transaction.objectStore('restaurants');
+      restaurantStore2.createIndex('cuisine_type', 'cuisine_type');
+      restaurantStore2.createIndex('neighborhood', 'neighborhood');
+
   }
 });
 
